8a562fd6c94ff938416f2906cb3c72b2b6cd40b7
M-Mafeni <mm17291@bristol.ac.uk>
2020-07-11 17:16:20 +0100

    Added board component

---
+++ src/Board.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Cell} from './Cell';
+export class Board extends React.Component{
+    makeRow(){
+        let row = Array(7).fill(<Cell/>);
+        console.log(row)
+        return <div className="row">{row}</div>;   
+    }
+    render(){
+        let grid = new Array(6);
+        for (let i = 0; i < grid.length; i++) {  
+            let row = new Array(7);          
+            for(let j = 0; j < 7; j++){
+                // let id = i.toString() + j.toString();
+                // console.log(id);
+                let elem = (
+                    <Cell key = {j}/>
+                );
+                row[j] = elem;
+            }
+            grid[i] = <div className="row" key = {i}>{row}</div>
+        }
+        // console.log(row);
+        return (
+            <div>
+                {grid}
+            </div>
+        );
+    }  
+}
