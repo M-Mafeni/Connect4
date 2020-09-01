f8261bc9c6d37008598d533d3055ce72c40b6b50
M-Mafeni <mafeni99@gmail.com>
2020-09-01 16:41:10 +0100

    converted column into a functional component

--- src/Column.js
+++ src/Column.js
@@ -2,40 +2,35 @@ import React from 'react';
 import {Cell} from './Cell';
 import {colors} from './Color'
 
-export class Column extends React.Component{
-    constructor(props){
-        super(props);
-        // this.state = {vals:this.props.vals,turn:this.props.turn}
-        this.handleClick= this.handleClick.bind(this)
-    }
-    handleClick(){
+export function Column(props){
+    function handleClick(){
         //starting from the bottom find vacant cell and change its color
-        if(!(this.props.winner)){
-            let cells = this.props.vals;
-            let turn = this.props.turn;
+        if(!(props.winner)){
+            let cells = props.vals;
+            let turn = props.turn;
             for (let index = 5; index >= 0; index--) {
                 if(cells[index]===colors.vacant){
                     cells[index]= turn ? colors.p1: colors.p2;
                     break;
                 }
             }
-            this.props.onClick();
+            props.onClick();
             // console.log(cells)
             // this.setState({vals:cells, turn:!turn})
         }
 
     }
-    render(){
-        // console.log(this.props.onClick)
-        let cells = Array(6);
-        for(let i = 0; i < 6; i++){
-            let color = this.props.vals[i]
-            cells[i]= <Cell key={i} color = {color}></Cell>
-        }
-        return (
-            <div onClick = {this.handleClick} className="column">
-                {cells}
-            </div>
-        );
+
+    let cells = Array(6);
+    for(let i = 0; i < 6; i++){
+        let color = props.vals[i]
+        cells[i]= <Cell key={i} color = {color}/>
     }
+        // console.log(this.props.onClick)
+
+    return (
+        <div onClick = {handleClick} className="column">
+            {cells}
+        </div>
+    );
 }
