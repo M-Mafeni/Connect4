9ac8a652cd6e10d84d546039bf17413774c0a8b7
M-Mafeni <mafeni99@gmail.com>
2020-09-01 15:19:47 +0100

    made Cell into a functional component

--- src/Cell.js
+++ src/Cell.js
@@ -1,15 +1,9 @@
 import React from 'react';
-// import {colors} from './Color'
 
-export class Cell extends React.Component{
-    // constructor(props){
-    //     super(props)
-    // }
-    render(){
-        let customStyle = {
-            backgroundColor: this.props.color
-        };
-        return (<div className = "cell" style={customStyle} > 
-        </div>);
-    }
+export function Cell(props){
+    let customStyle = {
+        backgroundColor: props.color
+    };
+    return (<div className = "cell" style={customStyle} >
+    </div>);
 }
